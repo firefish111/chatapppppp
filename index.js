ef37b575f3f528237a96c0cbd64b6afba0d71dfd
tayinde <tbiayinde@gmail.com>
2020-03-21 12:08:08 -0400

    first commit commit xP

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
