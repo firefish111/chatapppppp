77ff792a27c258ad4f868432c02b759200990eff
tayinde <tbiayinde@gmail.com>
2020-03-21 11:55:35 -0400

    first commit commit xP

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+io.on('connection', function(socket){
+	io.emit('connected');
+	socket.on('chat message', function(msg, user){
+		io.emit('chat message', msg, user);
+		console.log(user + ': ' + msg);
+	});
+	socket.on('sign in', function(username){
+		if ((username.search(' ') == -1) && (username !== "")) {
+			io.emit('sign in', username);
+		}
+	})
+});
